b736f570dcaba5e4d73a1cd34bac0b23b927ac3a
Himanshu Rane <himanshurane6501@gmail.com>
2023-02-06 13:10:39 +0530

    login and register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
     <div>
-      <Profile></Profile>
+      <Register></Register>
+      {/* <Login></Login> */}
+      {/* <Profile></Profile> */}
       {/* <Home></Home> */}
     </div>
   );
