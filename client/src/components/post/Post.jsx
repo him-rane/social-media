2be164c7c46ecae054d4a502968b52806d5d4a4e
Himanshu Rane <himanshurane6501@gmail.com>
2023-02-06 12:37:04 +0530

    Profile Pricture

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./post.css";
 import { MoreVert } from "@mui/icons-material";
 import { Users } from "../../dummyData";
 
 const Post = ({ post }) => {
-  console.log(post);
+  const [like, setLike] = useState(post.like);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const likeHandler = () => {
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsLiked(!isLiked);
+  };
   return (
     <div className="post">
       <div className="postWrapper">
@@ -34,13 +40,15 @@ const Post = ({ post }) => {
               className="likeIcon"
               src="https://cdn-icons-png.flaticon.com/512/889/889140.png"
               alt=""
+              onClick={likeHandler}
             />
             <img
               className="likeIcon"
               src="https://cdn-icons-png.flaticon.com/512/2589/2589054.png"
               alt=""
+              onClick={likeHandler}
             />
-            <span className="likeCounter">{post.like} People Like it</span>
+            <span className="likeCounter">{like} People Like it</span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentText">{post.comment} Comments</span>
--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -1,36 +1,105 @@
-import React from "react";
 import "./rightbar.css";
-import Online from "../online/Online";
 import { Users } from "../../dummyData";
+import Online from "../online/Online";
 
-const Rightbar = () => {
-  return (
-    <div className="rightbar">
-      <div className="rightbarWrapper">
+export default function Rightbar({ profile }) {
+  const HomeRightbar = () => {
+    return (
+      <>
         <div className="birthdayContainer">
-          <img
-            className="birthdayImg"
-            src="https://cdn-icons-png.flaticon.com/512/837/837891.png"
-            alt=""
-          />
+          <img className="birthdayImg" src="assets/gift.png" alt="" />
           <span className="birthdayText">
             <b>Pola Foster</b> and <b>3 other friends</b> have a birhday today.
           </span>
         </div>
-        <img
-          className="rightbarAd"
-          src="https://img.freepik.com/free-vector/flat-golden-circle-balloons-birthday-background_52683-34659.jpg?w=900&t=st=1675656500~exp=1675657100~hmac=3a59a5aafd4e18447fa1e6a2e42183e6645c816e95f8ce3742dbf0e72624cc3a"
-          alt=""
-        />
+        <img className="rightbarAd" src="assets/ad.png" alt="" />
         <h4 className="rightbarTitle">Online Friends</h4>
         <ul className="rightbarFriendList">
           {Users.map((u) => (
             <Online key={u.id} user={u} />
           ))}
         </ul>
+      </>
+    );
+  };
+
+  const ProfileRightbar = () => {
+    return (
+      <>
+        <h4 className="rightbarTitle">User information</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">City:</span>
+            <span className="rightbarInfoValue">New York</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">From:</span>
+            <span className="rightbarInfoValue">Madrid</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">Relationship:</span>
+            <span className="rightbarInfoValue">Single</span>
+          </div>
+        </div>
+        <h4 className="rightbarTitle">User friends</h4>
+        <div className="rightbarFollowings">
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/1.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/2.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/3.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/4.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/5.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/6.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+        </div>
+      </>
+    );
+  };
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        {profile ? <ProfileRightbar /> : <HomeRightbar />}
       </div>
     </div>
   );
-};
-
-export default Rightbar;
+}
